4350fb4edc936a1f636d2143bd4b7b432a16a9dc
Solomon S <slm4996+github@gmail.com>
2020-01-08 08:31:14 -0500

    Only apply raspberry pi firmware if on raspbian

--- raspbernetes/conf/firmware.sh
+++ raspbernetes/conf/firmware.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-# Update firmware for Pi - Needed for CNI (weave) to work correctly
-sudo SKIP_WARNING=1 rpi-update
+if sudo grep -Fq 'Raspbian' /etc/os-release; then
+    # Update firmware for Pi - Needed for CNI (weave) to work correctly
+    sudo SKIP_WARNING=1 rpi-update
+fi
