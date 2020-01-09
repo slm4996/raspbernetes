132f8ae4e533b56f7b590c979c1efc628c5405a7
Solomon S <slm4996+github@gmail.com>
2020-01-09 12:13:11 -0500

    Fix scripts not running

--- raspbernetes/bootstrap.d/04-install_prerequisites.sh
+++ raspbernetes/bootstrap.d/04-install_prerequisites.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 set -euo pipefail
 
-function install_prerequisites() {
-    echo "Updating core packages..."
-    DEBIAN_FRONTEND=noninteractive apt-get update
+echo "Updating core packages..."
+DEBIAN_FRONTEND=noninteractive apt-get update
 
-    echo "Installing base packages..."
-    DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
-    apt-transport-https \
-    software-properties-common \
-    zip \
-    jq \
-    git \
-    vim \
-    curl
+echo "Installing base packages..."
+DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
+apt-transport-https \
+software-properties-common \
+zip \
+jq \
+git \
+vim \
+curl
 
-    DEBIAN_FRONTEND=noninteractive apt-get -y upgrade
-    reboot
-}
+DEBIAN_FRONTEND=noninteractive apt-get -y upgrade
+reboot
--- raspbernetes/bootstrap.d/05-configure_iptables.sh
+++ raspbernetes/bootstrap.d/05-configure_iptables.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 set -euo pipefail
 
-function configure_iptables() {
-    echo "Installing ebtables and arptables..."
-    DEBIAN_FRONTEND=noninteractive apt-get install -y ebtables arptables
+echo "Installing ebtables and arptables..."
+DEBIAN_FRONTEND=noninteractive apt-get install -y ebtables arptables
 
-    echo "Setting to use legacy tables for compatibility issues"
-    update-alternatives --set iptables /usr/sbin/iptables-legacy
-    update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
-    update-alternatives --set ebtables /usr/sbin/ebtables-legacy
-    update-alternatives --set arptables /usr/sbin/arptables-legacy
+echo "Setting to use legacy tables for compatibility issues"
+update-alternatives --set iptables /usr/sbin/iptables-legacy
+update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
+update-alternatives --set ebtables /usr/sbin/ebtables-legacy
+update-alternatives --set arptables /usr/sbin/arptables-legacy
 
-    echo "Enable passing bridged IPv4 traffic to iptables’ chains."
-    echo "This is a requirement for some CNI plugins to work."
-    sysctl net.bridge.bridge-nf-call-iptables=1
-    sysctl net.bridge.bridge-nf-call-ip6tables=1
-    sysctl net.bridge.bridge-nf-call-arptables=1
+echo "Enable passing bridged IPv4 traffic to iptables’ chains."
+echo "This is a requirement for some CNI plugins to work."
+sysctl net.bridge.bridge-nf-call-iptables=1
+sysctl net.bridge.bridge-nf-call-ip6tables=1
+sysctl net.bridge.bridge-nf-call-arptables=1
 
-    echo "Reloading sysctl"
-    modprobe br_netfilter
-    sysctl -p
-}
+echo "Reloading sysctl"
+modprobe br_netfilter
+sysctl -p
